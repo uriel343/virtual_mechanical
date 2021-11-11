b69e285e879ba21e783b5f8ea54f0b446dfa4aa0
Uriel Ortiz <uriel.ortiz@hugoapp.com>
2021-11-10 21:01:48 -0600

    Deploy

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const port = process.env.PORT || 8080;
+
+//creando aplicacion
+const app = express();
+
+app.use(express.static(__dirname + "/dist/"))
+app.get(/.*/, function(req,res) {
+    res.sendFile(__dirname + "/dist/index.html");
+})
+
+app.listen(port);
+console.log('Server started')
